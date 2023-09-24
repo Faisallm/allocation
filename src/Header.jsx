78cbd39a8418e947fb5d31861878904102006ebb
Faisallm <faisallawan1997@gmail.com>
2023-09-24 16:25:05 -0700

    coder coder fruit

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+// header component
+// arrow function syntax
+const Header = () => {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
